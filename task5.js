36497452ad7f9b667b493839294b3243a3dede1f
BhushanBhale27 <bhushan27official@gmail.com>
2023-06-29 18:30:58 +0530

    Task 5 (imp html css static rootDir)

--- task5.js
+++ task5.js
@@ -1,5 +1,4 @@
-
-const path = require('path')
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const express = require("express");
@@ -8,20 +7,20 @@ const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/Shop");
-const contactusRoutes = require("./routes/contactus")
+const contactusRoutes = require("./routes/contactus");
 
 app.use(bodyParser.urlencoded({ extended: false })); //it parse text related entries
-app.use(express.static(path.join(__dirname , 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use( "/admin" ,adminRoutes); // "/admin" is filtering routes
+app.use("/admin", adminRoutes); // "/admin" is filtering routes
 app.use(shopRoutes);
-app.use(contactusRoutes)
+app.use(contactusRoutes);
 
 //adding 404 page
 
-app.use((req,res,next)=>{
-  res.status(404).sendFile(path.join(__dirname,'views','pageNotFound.html'))
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "pageNotFound.html"));
   //status(404) is for network tab
-})
+});
 
-app.listen(3001)
+app.listen(3001);
